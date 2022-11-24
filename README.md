2bdcec5c8e6d7c641337bea417e734ed005431c9
Remo Inderbitzin <remo.inderbitzin@siemens.com>
2022-11-24 13:12:41 +0100

    feat: adapt to variete

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Rpisurv 3 - Raspberry Pi surveillance
+
 Join the community on https://community.rpisurv.net (Please use this place for support questions instead of github issues).<br/>
 Bug tracking https://github.com/SvenVD/rpisurv/issues.<br/>
 Follow us on facebook https://www.facebook.com/rpisurv.<br/>
 Have a chat on https://gitter.im/rpisurv/general.<br/>
 
- You can help sustain our activities by donating here:
- <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QPJU9K2KZ8D94" target="_blank" rel="nofollow"><img src="https://www.paypal.com/en_US/i/btn/x-click-but21.gif" alt="" /></a>
+You can help sustain our activities by donating here:
+<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QPJU9K2KZ8D94" target="_blank" rel="nofollow"><img src="https://www.paypal.com/en_US/i/btn/x-click-but21.gif" alt="" /></a>
 
 Or if you prefer crypto take a look at the funding page [here](FUNDING.md)
 
@@ -18,27 +19,35 @@ Take a look at some [showcases](https://www.tapatalk.com/groups/rpisurv/showcase
 ![Screenshot](https://gist.githubusercontent.com/SvenVD/84b230515e56bc7dca915731425ce437/raw/df7691cca60850a5b3864ce17cd8c9a8a73861ac/demo2images_andautocalculation_small.png)
 
 ## Features
+
 ##### Self-healing and health monitoring including watchdogs
+
 - Every stream will be monitored by an external watchdog process, if the stream gets killed somehow, the watchdog will try to restart the stream/process. This gives you a very robust surveillance screen.
-- Stream up/down detection and autorepositioning of connectable streams on the screen layout. 
+- Stream up/down detection and autorepositioning of connectable streams on the screen layout.
   For example: if you stop a camera (or just stop the server on the camera), Rpisurv will detect this and will recalculate/redraw the screen with the still available streams. The same is true if a previous unconnectable stream becomes connectable. All without any user interaction.
 
 ##### Automatically position streams (no manual coordinates calculation needed)
+
 - Autocalculcate coordinates for every stream displayed.
-- If you are not happy with the autocalculations you [can](https://gist.github.com/SvenVD/5c82acbb96dfb697e5e2d2420ab73ad9) customize yourself. 
+- If you are not happy with the autocalculations you [can](https://gist.github.com/SvenVD/5c82acbb96dfb697e5e2d2420ab73ad9) customize yourself.
 
 ##### Rotation of screens (autorotate or with keyboard control)
-- You can configure multiple screens and cycle between them in an automated way or via the keyboard. 
+
+- You can configure multiple screens and cycle between them in an automated way or via the keyboard.
 - In the case of dual displays, you can configure multiple screens to be cycled between for each display.
 
 ##### Multiple types of streams
+
 - You can also specify "image streams", to display images next to or instead of camera streams. The images will be auto-updated if they change remotely.
 
 ##### Dual Hdmi support
+
 - Rpisurv will auto-detect if a second display is connected at boot and will automatically start the configured screens for the second display.
 
 ## How to get started
+
 In short: The idea is to connect your Raspberry Pi to a monitor and tell Rpisurv which stream(s) and screen(s) it should display or cycle between. Rpisurv will autocalculate all the rest.
+
 - Get a monitor or a TV ( or 2 )
 - Get a Raspberry Pi dedicated for Rpisurv
 - Install Raspberry Pi OS **Full (Legacy)** on the Pi (Tested successfully with the full version, not lite)
@@ -54,11 +63,12 @@ In short: The idea is to connect your Raspberry Pi to a monitor and tell Rpisurv
 ## Configuration
 
 Rpisurv has the following config files
+
 - `/etc/rpisurv/general.yml` => General config, mostly not needed to touch this.
-- `/etc/rpisurv/display1.yml`  => Define screens and streams used for first monitor.
-- `/etc/rpisurv/display2.yml`  => Define screens and streams used for second monitor.
-Which monitor will be display1 or display2 depends on which HDMI port you plug it in.   
-If you only plug one monitor then only one of the config files will be read (display1.yml or display2.yml).
+- `/etc/rpisurv/display1.yml` => Define screens and streams used for first monitor.
+- `/etc/rpisurv/display2.yml` => Define screens and streams used for second monitor.
+  Which monitor will be display1 or display2 depends on which HDMI port you plug it in.  
+  If you only plug one monitor then only one of the config files will be read (display1.yml or display2.yml).
 
 A screen consists out of multiple streams.
 A display is the equivalent of a monitor. On a display you can define multiple screens which can be cycled between.
@@ -68,20 +78,26 @@ For full config explanation with all possible options consult the config files i
 ## URL sources
 
 #### file://
+
 This is a path on disk, by default a videofile is expected. This videofile will then be played in an endless loop.
 If used with imageurl: true then an image file is expected. If the image changes on disk then Rpisurv will also reload the stream with the new image.
 Note in dual displays mode imageurl type streams are only displayed on 1 display (`/etc/rpisurv/display1.yml`).
 TIP: If an external program rotates the images on disk then Rpisurv can thus be used as a frontend for a digital picture frame.
+
 #### http:// and https://
+
 This is a remote location with a video file or video stream
 If used with imageurl: true then an image file is expected. If the remote image changes then Rpisurv will also reload the stream with the new image.
 Note in dual displays mode imageurl type streams are only displayed on 1 display (`/etc/rpisurv/display1.yml`).
 TIP: This can be used as part of a digital signage setup: several Rpisurv clients can be steered centrally by changing the image(s) on the central location.
 Rpisurv will autodetect interruptions and tries to restore the stream.
+
 #### rtsp:// and rtmp://
+
 This is a remote location with a video stream, Rpisurv will do his best to display the stream, it will autodetect interruptions and tries to restore the stream.
 
 ## How to update Rpisurv to new version <a name="how-to-update"></a>
+
 - `cd rpisurv; git pull`
 - OPTIONAL: checkout a specific branch, for example `git checkout v3_latest`, if you want to override the default version on master
 - run `sudo ./install.sh` (The installer will ask if you want to preserve your current config file)
@@ -92,10 +108,12 @@ This is a remote location with a video stream, Rpisurv will do his best to displ
 See [RELEASE_NOTES](https://github.com/SvenVD/rpisurv/releases)
 
 ## Placeholder images
+
 After installation you may change the placeholder images to something you like.
-- /usr/local/bin/rpisurv/images/connecting.png is shown when a camera stream is starting up
-- /usr/local/bin/rpisurv/images/placeholder.png is shown on empty squares
-- /usr/local/bin/rpisurv/images/noconnectable.png is shown full screen when no connectable streams are detected for the current active screen in case multiple are cycled between
+
+- /usr/local/bin/rpisurv/images/connecting.jpg is shown when a camera stream is starting up
+- /usr/local/bin/rpisurv/images/placeholder.jpg is shown on empty squares
+- /usr/local/bin/rpisurv/images/noconnectable.jpg is shown full screen when no connectable streams are detected for the current active screen in case multiple are cycled between
 - `systemctl restart rpisurv`
 
 ## Rpisurv in operation
@@ -106,17 +124,17 @@ If you are connected via keyboard/keypad, you can force the next screen by press
 
 Keys F1 to F12 (or keypad 0 to 9), will force the equal numbered screen to be shown onscreen (this takes longer depending on amount of unconnectable streams and they thus need to wait for timeout, keep holding until screen changes. Note, you can change probe_timeout per camera stream if needed).
 
-Disable rotation (as in pause rotation, as in fix the current displayed screen) dynamically during runtime. By pressing "p" (or keypad "*") to pause or "r" (or "," or keypad ".")' to resume/rotate. This overrides the disable_autorotation option if this has been set in the config file.
+Disable rotation (as in pause rotation, as in fix the current displayed screen) dynamically during runtime. By pressing "p" (or keypad "\*") to pause or "r" (or "," or keypad ".")' to resume/rotate. This overrides the disable_autorotation option if this has been set in the config file.
 
 Touchscreen control:
-The width of the screen is divided in four sections, 
+The width of the screen is divided in four sections,
+
 - Touching on the first section trigger a pause event.
 - Touching In the two sections in the middle trigger a resume event.
 - Touching In the last section, a next screen event.
-Note that a mouse can be used, however mouse cursor is hidden by default.
+  Note that a mouse can be used, however mouse cursor is hidden by default.
 
 In case of dual HDMI then the screens on both displays will be controlled at the same time.
-
 
 ## Troubleshooting
 
@@ -127,6 +145,7 @@ In case of dual HDMI then the screens on both displays will be controlled at the
 - If you are connected via keyboard/keypad, you can stop Rpisurv by pressing and holding q (or backspace or keypad "/") (this can take some seconds) .
 
 - To manage the screen without rebooting use systemctl
+
   - `sudo systemctl stop rpisurv` to stop the screen
   - `sudo systemctl start rpisurv` to start the screen
   - `sudo systemctl status rpisurv` to see last log and status of service
@@ -138,7 +157,6 @@ In case of dual HDMI then the screens on both displays will be controlled at the
 - On a Raspberry Pi 3 and 4 it seems the default overscan settings are not good. If full screen is not used, if you have an unused bar in the bottom -> try to set `disable_overscan=1` in /boot/config.txt
 
 - Users on a Raspberry Pi 4 which experience flickering can try to set `disable_overscan=1` in /boot/config.txt. As reported here: [link](https://www.tapatalk.com/groups/rpisurv/camera-flickering-not-sure-what-is-the-issue-t48.html#p192).
-
 
 ## Feature requests
 
--- surveillance/core/Screen.py
+++ surveillance/core/Screen.py
@@ -5,40 +5,42 @@ import collections
 
 from .CameraStream import CameraStream
 
-#We do not need exact floating point numbers, use builtin "float" instead
+# We do not need exact floating point numbers, use builtin "float" instead
 #from decimal import getcontext, Decimals
 
 
 logger = logging.getLogger('l_default')
 
+
 class Screen:
     """This class creates and handles camerastreams objects and their position"""
+
     def __init__(self, screenname, screen_cfg, display, drawinstance):
-        ## Init vars
-        #Use vcgencmd dispmanx_list to see on which layer framebuffer is located
-        #Highest observed was 2147483647 on a rpi4, start high with some tolerance,so caching has a long way to count down before it needs to reset the layer
-        self.layer_init_value=2000000000
-        self.layer=self.layer_init_value
-        self.display_vlc_hdmi_id=str(int(display["hdmi"]) + 1)
-        self.disable_probing_for_all_streams = screen_cfg.setdefault('disable_probing_for_all_streams', False)
+        # Init vars
+        # Use vcgencmd dispmanx_list to see on which layer framebuffer is located
+        # Highest observed was 2147483647 on a rpi4, start high with some tolerance,so caching has a long way to count down before it needs to reset the layer
+        self.layer_init_value = 2000000000
+        self.layer = self.layer_init_value
+        self.display_vlc_hdmi_id = str(int(display["hdmi"]) + 1)
+        self.disable_probing_for_all_streams = screen_cfg.setdefault(
+            'disable_probing_for_all_streams', False)
         self.nr_of_columns = screen_cfg.setdefault('nr_of_columns', 2)
         self.name = screenname
         self.screen_cfg = screen_cfg
         self.camera_streams_cfg = screen_cfg["camera_streams"]
         self.duration = self.screen_cfg.setdefault('duration', 30)
-        logger.debug("Screen: " + self.name + " duration from config is: " + str(self.duration))
+        logger.debug("Screen: " + self.name +
+                     " duration from config is: " + str(self.duration))
         self.drawinstance = drawinstance
         self.first_run = True
         self.start_of_active_time = -1
         self.previous_connectable_camera_streams = []
 
-
-        ## Init functions
+        # Init functions
         # Sets internal variable resolution
         self.resolution_width = int(display["resolution"]["width"])
         self.resolution_height = int(display["resolution"]["height"])
         self._init_camera_streams()
-
 
     def has_image_url(self):
         """Returns True if this screen has at least one stream that is an imageurl"""
@@ -46,7 +48,7 @@ class Screen:
         for camera_stream in self.all_camera_streams:
             if camera_stream.is_imageurl():
                 logger.debug(
-                    "Screen: " + self.name + " has_image_url: detected at least one imageurl: " + camera_stream.name )
+                    "Screen: " + self.name + " has_image_url: detected at least one imageurl: " + camera_stream.name)
                 return True
         return False
 
@@ -57,13 +59,15 @@ class Screen:
         for camera_stream in self.camera_streams_cfg:
             counter = counter + 1
             cam_stream_name = self.name + "_cam_stream" + str(counter)
-            cam_stream = CameraStream(cam_stream_name, camera_stream, self.drawinstance, self.display_vlc_hdmi_id)
+            cam_stream = CameraStream(
+                cam_stream_name, camera_stream, self.drawinstance, self.display_vlc_hdmi_id)
             self.all_camera_streams.append(cam_stream)
 
-    def update_connectable_camera_streams(self, skip = False):
+    def update_connectable_camera_streams(self, skip=False):
         '''Filters a list of camerastream instances into a new list of connectable camera_streams'''
         if skip:
-            logger.debug("Screen: " + self.name + " Skipping checking connectable cameras, skip value is: " + str(skip))
+            logger.debug("Screen: " + self.name +
+                         " Skipping checking connectable cameras, skip value is: " + str(skip))
             # Sometimes we do not want to wait on checking connectable camera streams, so this shortcut is taken (essentially skip checking)
             if self.first_run:
                 # We need the camera stream to be initialised with something in the beginning if we decide to skip the checking of connectivity
@@ -73,15 +77,15 @@ class Screen:
             else:
                 self.connectable_camera_streams = self.previous_connectable_camera_streams
         else:
-            logger.debug("Screen: Start polling connectivity for the camera_streams part of screen: " + self.name)
-            self.connectable_camera_streams=[]
+            logger.debug(
+                "Screen: Start polling connectivity for the camera_streams part of screen: " + self.name)
+            self.connectable_camera_streams = []
             for cam_stream in self.all_camera_streams:
-                    if cam_stream.is_connectable():
-                        self.connectable_camera_streams.append(cam_stream)
+                if cam_stream.is_connectable():
+                    self.connectable_camera_streams.append(cam_stream)
 
         self.cam_streams_to_draw = self.connectable_camera_streams
         self.cam_streams_to_stop = self.previous_connectable_camera_streams
-
 
     def destroy(self):
         logger.debug("Screen: Destroying screen: " + self.name)
@@ -94,27 +98,31 @@ class Screen:
         self.first_run = True
         self.start_of_active_time = -1
 
-    def set_layer(self,layer):
+    def set_layer(self, layer):
         self.layer = layer
-        logger.debug("Screen: layer for screen " + self.name + " has been set to " + str(self.layer))
+        logger.debug("Screen: layer for screen " + self.name +
+                     " has been set to " + str(self.layer))
 
     def get_layer(self):
         return self.layer
 
     def reset_layer(self):
-        logger.debug("Screen: layer for screen " + self.name + " will be reset to " + str(self.layer_init_value) + " coming from " + str(self.layer))
-        self.layer=self.layer_init_value
+        logger.debug("Screen: layer for screen " + self.name + " will be reset to " +
+                     str(self.layer_init_value) + " coming from " + str(self.layer))
+        self.layer = self.layer_init_value
 
     def reset_active_timer(self):
         logger.debug("Screen: reset_active_timer " + self.name)
-        #Set start time
+        # Set start time
         self.start_of_active_time = time.time()
-        logger.debug("Screen: " + self.name + " start_of_active_time: " + str(self.start_of_active_time))
+        logger.debug("Screen: " + self.name +
+                     " start_of_active_time: " + str(self.start_of_active_time))
 
     def get_active_run_time(self):
         '''Returns how long the screen is in active mode'''
         active_run_time = int(round((time.time() - self.start_of_active_time)))
-        logger.debug("Screen: " + self.name + " active_run_time: " + str(active_run_time) + " / " + str(self.duration))
+        logger.debug("Screen: " + self.name + " active_run_time: " +
+                     str(active_run_time) + " / " + str(self.duration))
         return active_run_time
 
     def _is_connectable_streams_changed(self):
@@ -130,80 +138,82 @@ class Screen:
         # Only re-draw screen if something is changed or try redrawing if there is no camerastream that is connectable OR if we change the screen
         if self._is_connectable_streams_changed() or len(self.previous_connectable_camera_streams) == 0:
             logger.debug(f"Screen {self.name} needs update/redraw: changes in connectable camera streams detected.( previous: {len(self.previous_connectable_camera_streams)} / now: {len(self.connectable_camera_streams)} or different connectable streams then before )")
-            #Stop all running streams before redrawing
+            # Stop all running streams before redrawing
             for cam_stream_to_stop in self.cam_streams_to_stop:
                 cam_stream_to_stop.stop_stream()
 
-
-            nr_of_columns=int(self.nr_of_columns)
+            nr_of_columns = int(self.nr_of_columns)
 
             # Start algorithm to start new streams
             fields = len(self.cam_streams_to_draw)
-            logger.debug( "Screen: " + self.name + " number of fields= " + str(fields))
+            logger.debug("Screen: " + self.name +
+                         " number of fields= " + str(fields))
 
             if fields == 0:
-                #Draw no connectable placeholder
-                self.drawinstance.placeholder(0, 0, self.resolution_width, self.resolution_height, "images/noconnectable.png")
+                # Draw no connectable placeholder
+                self.drawinstance.placeholder(
+                    0, 0, self.resolution_width, self.resolution_height, "images/noconnectable.jpg")
                 self.previous_connectable_camera_streams = self.connectable_camera_streams
                 return
 
-
-            #If you have less fields than columns then only set fields amount columns
+            # If you have less fields than columns then only set fields amount columns
             if fields <= nr_of_columns:
-                nr_of_columns=fields
+                nr_of_columns = fields
 
-            #We calculate needed numbers of rows based on how many fields we have and how many columns per row we want
-            nr_of_rows=math.ceil(float(fields)/nr_of_columns)
+            # We calculate needed numbers of rows based on how many fields we have and how many columns per row we want
+            nr_of_rows = math.ceil(float(fields)/nr_of_columns)
 
-            default_fieldwidth=int(self.resolution_width/nr_of_columns)
-            default_fieldheight=int(self.resolution_height/nr_of_rows)
+            default_fieldwidth = int(self.resolution_width/nr_of_columns)
+            default_fieldheight = int(self.resolution_height/nr_of_rows)
 
-            normal_fieldwidth=default_fieldwidth
-            normal_fieldheight=default_fieldheight
+            normal_fieldwidth = default_fieldwidth
+            normal_fieldheight = default_fieldheight
 
-            currentrow=1
-            currentwindow=1
-            currentrowlength=nr_of_columns
+            currentrow = 1
+            currentwindow = 1
+            currentrowlength = nr_of_columns
 
-            x1=0
-            y1=0
-            x2=normal_fieldwidth
-            y2=normal_fieldheight
+            x1 = 0
+            y1 = 0
+            x2 = normal_fieldwidth
+            y2 = normal_fieldheight
 
             for cam_stream in self.cam_streams_to_draw:
 
                 if currentwindow > currentrowlength:
-                    #This is a new row event
-                    x1=0
-                    x2=normal_fieldwidth
-                    y1=y1 + normal_fieldheight
-                    y2=y2 + normal_fieldheight
+                    # This is a new row event
+                    x1 = 0
+                    x2 = normal_fieldwidth
+                    y1 = y1 + normal_fieldheight
+                    y2 = y2 + normal_fieldheight
 
                     currentrow = currentrow + 1
-                    #Next row ends when we are at following currentwindow index number
+                    # Next row ends when we are at following currentwindow index number
                     currentrowlength = currentrowlength + nr_of_columns
                 else:
-                    #This is a window in the same row
-                    x1=x1 + normal_fieldwidth
-                    x2=x2 + normal_fieldwidth
+                    # This is a window in the same row
+                    x1 = x1 + normal_fieldwidth
+                    x2 = x2 + normal_fieldwidth
 
-                #If this is the first field/window override some settings
+                # If this is the first field/window override some settings
                 if currentwindow == 1:
-                    x1=0
-                    x2=normal_fieldwidth
+                    x1 = 0
+                    x2 = normal_fieldwidth
 
                 if currentwindow == fields:
-                    #Start calculation to display placeholders
+                    # Start calculation to display placeholders
                     free_horizontal_pixels = self.resolution_width - x2
-                    #If we have some unused screen space. Start drawing placeholders to fill the free space
+                    # If we have some unused screen space. Start drawing placeholders to fill the free space
                     if free_horizontal_pixels > 0:
-                        logger.debug("Screen: We have " + str(free_horizontal_pixels) + " free_horizontal_pixels unused screen. Start drawing placeholders to fill the free space")
-                        nr_of_placeholders=free_horizontal_pixels/normal_fieldwidth
+                        logger.debug("Screen: We have " + str(free_horizontal_pixels) +
+                                     " free_horizontal_pixels unused screen. Start drawing placeholders to fill the free space")
+                        nr_of_placeholders = free_horizontal_pixels/normal_fieldwidth
                         count_placeholders = 0
                         placeholder_x = x1 + normal_fieldwidth
                         placeholder_y = y1
                         while count_placeholders < nr_of_placeholders:
-                            self.drawinstance.placeholder(placeholder_x, placeholder_y, normal_fieldwidth, normal_fieldheight, "images/placeholder.png")
+                            self.drawinstance.placeholder(
+                                placeholder_x, placeholder_y, normal_fieldwidth, normal_fieldheight, "images/placeholder.jpg")
                             count_placeholders = count_placeholders + 1
                             placeholder_x = placeholder_x + normal_fieldwidth
 
@@ -213,8 +223,8 @@ class Screen:
                 # y1 #y coordinate upper left corner
                 # x2 #x coordinate absolute where window should end, count from left to right
                 # y2 #y coordinate from where window should end, count from top to bottom of screen
-                cam_stream.start_stream([x1,y1,x2,y2], self.layer)
-                #Debug test
+                cam_stream.start_stream([x1, y1, x2, y2], self.layer)
+                # Debug test
                 #cam_stream.set_videopos([x1 + 50 ,y1 +50 ,x2 +50 ,y2 +50])
 
                 currentwindow = currentwindow + 1
@@ -225,10 +235,9 @@ class Screen:
 
         self.previous_connectable_camera_streams = self.connectable_camera_streams
 
-        #refresh all placeholders that were created on the screen itself.
-        #If there are imageurls then now is the time to refresh them all
+        # refresh all placeholders that were created on the screen itself.
+        # If there are imageurls then now is the time to refresh them all
         for cam_stream in self.cam_streams_to_draw:
             cam_stream.refresh_image_from_url()
 
         self.drawinstance.refresh()
-
